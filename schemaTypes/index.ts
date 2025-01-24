515d6d64b64963e79a386da93f7d0623ecca9ed4
Gabriele Nunez <gabrielenunez@protonmail.com>
2025-01-23 20:33:09 -0500

    Added additional document types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import {SchemaPluginOptions, Template} from 'sanity'
+import {productSchema} from './documents/product'
+import {postType} from './postType'
+
+export const schemaTypes = [postType, productSchema];
+export const templates = (templates: Template<string,string>[]) => templates.filter((template) => template.schemaType !== 'product');
